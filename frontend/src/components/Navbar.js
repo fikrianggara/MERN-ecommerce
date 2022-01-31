dd1295a21437e7fba77879038c2321ca0380d874
fikrianggara <221810306@stis.ac.id>
2022-02-01 06:53:42 +0700

    initialize project codebase with react and tailwind

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const Container = styled.div.attrs({
+    className:"text-center bg-purple-400 p-4"
+})``
+
+
+const Navbar = () => {
+  return <Container>
+      navbar
+  </Container>;
+};
+
+export default Navbar;
