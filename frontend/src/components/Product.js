cb884ecc0d918384f63babdbd3a28a7c906a065c
fikrianggara <221810306@stis.ac.id>
2022-02-07 05:36:04 +0700

    update fe

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,29 +4,32 @@ import styled from 'styled-components';
 import {FavoriteBorderOutlined, ShoppingCartOutlined, SearchOutlined} from '@mui/icons-material';
 
 const ProductItem = styled.div.attrs({
-  className:' w-full h-full bg-cover flex rounded-lg'
+  className:' w-full h-full bg-cover flex rounded-lg transition'
 })`background-image:url('${props=>props.product.src}')`;
 const Action = styled.div.attrs({
-  className:'z-10 absolute flex flex-col p-2 md:p-4 h-1/2 bottom-0 right-0 md:p-0 text-gray-600 bg-white opacity-80 md:opacity-60 justify-between lg:justify-evenly rounded-lg rounded-tr-none md:rounded-bl-none'
+  className:'z-10 absolute flex flex-col p-2 md:p-4 h-1/2 bottom-0 transition right-0 md:p-4 text-gray-600 bg-white opacity-50 justify-between lg:justify-evenly rounded-lg rounded-tr-none md:rounded-bl-none'
 })``;
 const Container = styled.div.attrs({
   className:'relative rounded-lg w-full aspect-[3/4] shadow-lg flex'
 })`
-&:hover{
     background:black;
+&:hover{
+    transition: all 0.3s ease-in-out
 }
 &:hover ${ProductItem}{
+    transition: all 0.5s ease-in-out;
     opacity:0.6;
 }
 &:hover ${Action}{
-    opacity:0.8;
+    transition: all 0.5s ease-in-out;
+    opacity:1;
 }
 `;
 
 // 
 
 const Icon = styled.div.attrs({
-    className:'hover:cursor-pointer hover:text-green-600 md:m-2'
+    className:'hover:cursor-pointer hover:text-green-600 md:m-2 duration-300 ease-in-out'
 })``;
 const Template = styled.div.attrs({
   className:''
